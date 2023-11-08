e334778373d7148c929d7ac00da44adfc3e4903f
delanoquirino <delanoquirino@gmail.com>
2023-11-07 22:45:13 -0300

    feat: button icon search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,20 @@
+
 import { DistrictDropdown } from "./DistrictDropdown";
 import { PropertyDropdown } from "./PropertyDropdown";
 import { PriceRangeDropdownt } from "./PriceRangeDropdownt";
-import { RiSearch2Line } from 'react-icons/ri';
+import { RiSearch2Line } from "react-icons/ri";
+
 export const Search = () => {
+  
+
   return (
     <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur rounded-lg">
       <DistrictDropdown />
       <PropertyDropdown />
       <PriceRangeDropdownt />
-      <button className="bg-emerald-700 hover:bg-emerald-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg"><RiSearch2Line/></button>
+      <button className="bg-emerald-700 hover:bg-emerald-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg">
+        <RiSearch2Line />
+      </button>
     </div>
   );
 };
